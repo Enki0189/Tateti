3939344fb6cbbc1e29c0b05eb885f92cda57c30a
Rocío Catalano <rociocatalano4@hotmail.com>
2022-11-25 23:23:33 -0300

    cambiando métodos para que aparezcan en inglés, parte 1

--- src/juegoTateti/JuegoTateti.java
+++ src/juegoTateti/JuegoTateti.java
@@ -4,24 +4,25 @@ import java.sql.Connection;
 import java.sql.DriverManager;
 import java.sql.ResultSet;
 import java.sql.SQLException;
+import java.sql.Statement;
 import java.time.LocalDateTime;
 import java.util.Scanner;
 
 public class JuegoTateti {
 
-    //Datos conexion base de datos
-    public static final String URL_DB = "jdbc:mysql://127.0.0.1:3306/tateti";
+    // Datos conexion base de datos
+    public static final String URL_DB = "jdbc:mysql://localhost:3306/tateti";
     public static final String USUARIO_DB = "root";
     public static final String PASSWORD_DB = "******";
 
-    private static final int CODIGO_IDIOMA_ESP = 1;
+    private static final int CODIGO_IDIOMA_ESP = 1;    
 
     public static final char FICHA_JUGADOR = 'X';
     public static final char FICHA_COMPUTADORA = 'O';
 
     private static Scanner lector = new Scanner(System.in);
 
-    private static int codigoLenguajeSeleccionado = CODIGO_IDIOMA_ESP;  //Español por defecto
+    private static int codigoLenguajeSeleccionado = CODIGO_IDIOMA_ESP; // Español por defecto
 
     public static void main(String[] args) {
         try {
@@ -31,20 +32,25 @@ public class JuegoTateti {
 
             boolean terminar = false;
 
-            while(!terminar) {
-                mostrarMenuPrincipal();
-                int opcionElegida = leerOpcion();
+            while (!terminar) {
+                mostrarMenuPrincipal(miConexion);
+                int opcionElegida = leerOpcion(miConexion);
 
-                switch(opcionElegida) {
-                    case 1: mostrarIdiomasDisponibles(miConexion);
+                switch (opcionElegida) {
+                    case 1:
+                        mostrarIdiomasDisponibles(miConexion);
                         break;
-                    case 2: jugar(miConexion);
+                    case 2:
+                        jugar(miConexion);
                         break;
-                    case 3: mostrarPartidasJugador(miConexion);
+                    case 3:
+                        mostrarPartidasJugador(miConexion);
                         break;
-                    case 4: agregarMensajeNuevo(miConexion);
+                    case 4:
+                        agregarMensajeNuevo(miConexion);
                         break;
-                    case 5: terminar = true;
+                    case 5:
+                        terminar = true;
                     default:
                         break;
                 }
@@ -54,41 +60,57 @@ public class JuegoTateti {
             e.printStackTrace();
         }
     }
+    
+    private static ResultSet generarMensajes(int codigoLenguajeSeleccionado, Connection miConexion) throws SQLException {
+    	Statement statement = miConexion.createStatement(ResultSet.TYPE_SCROLL_SENSITIVE, ResultSet.CONCUR_READ_ONLY);
+    	ResultSet resultSetMensajes = statement.executeQuery("SELECT * FROM mensajes WHERE cod_idioma = " + codigoLenguajeSeleccionado);
+    	return resultSetMensajes;
+    }
 
-    private static int leerOpcion() {
+    private static int leerOpcion(Connection miConexion) throws SQLException {
+    	ResultSet resultSetMensajes = generarMensajes(codigoLenguajeSeleccionado, miConexion);
+    	resultSetMensajes.absolute(25);
         int opcionElegida = lector.nextInt();
-        while(opcionElegida < 1 || opcionElegida > 5) {
-            System.out.println("La opcion ingresada es incorrecta.");
-            mostrarMenuPrincipal();
+        while (opcionElegida < 1 || opcionElegida > 5) {
+        	System.out.println(resultSetMensajes.getString("mensaje"));
+			mostrarMenuPrincipal(miConexion);
             opcionElegida = lector.nextInt();
         }
         return opcionElegida;
     }
 
-    private static void mostrarMenuPrincipal() {
+    private static void mostrarMenuPrincipal(Connection miConexion) throws SQLException {
+    	ResultSet resultSetMensajes = generarMensajes(codigoLenguajeSeleccionado, miConexion);
         System.out.println();
-        System.out.println("1 - Cambiar idioma.");
-        System.out.println("2 - Jugar partida.");
-        System.out.println("3 - Ver resultados generales.");
-        System.out.println("4 - Ver resultados por jugador.");
-        System.out.println("5 - Salir");
+        
+        for (int i = 13; i <18; i++) {
+        	resultSetMensajes.absolute(i);
+        	System.out.println((i-12)+ " - " + resultSetMensajes.getString("mensaje"));
+        }
+
         System.out.println();
     }
 
     private static void mostrarPartidasJugador(Connection miConexion) throws SQLException {
+    	ResultSet resultSetMensajes = generarMensajes(codigoLenguajeSeleccionado, miConexion);
+    	resultSetMensajes.absolute(26);
         System.out.println();
-        System.out.println("Ingrese nombre del jugador: ");
+        System.out.println(resultSetMensajes.getString("mensaje"));
 
-        //Por alguna razon el siguiente nextLine el programa lo ignora y tuve que ponerlo 2 veces
+        // Por alguna razon el siguiente nextLine el programa lo ignora y tuve que
+        // ponerlo 2 veces
         lector.nextLine();
         String nombreDeJugador = lector.nextLine();
 
         System.out.println();
 
-        ResultSet miResultSet = miConexion.createStatement().executeQuery("SELECT * FROM info_partidas WHERE nombre_jugador = '" + nombreDeJugador + "'");
+        ResultSet miResultSet = miConexion.createStatement()
+                .executeQuery("SELECT * FROM info_partidas WHERE nombre_jugador = '" + nombreDeJugador + "'");
 
-        while(miResultSet.next()) {
-            System.out.println(miResultSet.getString("id_partida") + " " + miResultSet.getString("nombre_jugador") + " " + miResultSet.getString("inicio_partida") + " " + miResultSet.getString("fin_partida") + " " + miResultSet.getString("ganador") + " " + miResultSet.getString("idioma_elegido"));
+        while (miResultSet.next()) {
+            System.out.println(miResultSet.getString("id_partida") + " " + miResultSet.getString("nombre_jugador") + " "
+                    + miResultSet.getString("inicio_partida") + " " + miResultSet.getString("fin_partida") + " "
+                    + miResultSet.getString("ganador") + " " + miResultSet.getString("idioma_elegido"));
         }
     }
 
@@ -102,27 +124,49 @@ public class JuegoTateti {
         int codigoDeMensaje = lector.nextInt();
         System.out.println();
 
-        ResultSet miResultSet = miConexion.createStatement().executeQuery("SELECT * FROM mensajes where cod_idioma = " + codigoIdiomaSeleccionado + " AND cod_mensaje = " + codigoDeMensaje);
+        ResultSet miResultSet = miConexion.createStatement().executeQuery("SELECT * FROM mensajes where cod_idioma = "
+                + codigoIdiomaSeleccionado + " AND cod_mensaje = " + codigoDeMensaje);
 
-        //4- Recorrer el ResultSet
-        while(miResultSet.next()) {
+        // 4- Recorrer el ResultSet
+        while (miResultSet.next()) {
             System.out.println(miResultSet.getString("mensaje"));
         }
 
     }
 
     private static void mostrarIdiomasDisponibles(Connection miConexion) throws SQLException {
+    	ResultSet resultSetMensajes = generarMensajes(codigoLenguajeSeleccionado, miConexion);
+    	resultSetMensajes.absolute(23);
+    	System.out.println();
+        System.out.println(resultSetMensajes.getString("mensaje"));
         System.out.println();
-        System.out.println("Idiomas disponibles: ");
-        System.out.println();
-
-        ResultSet miResultSet = miConexion.createStatement().executeQuery("SELECT * FROM tabla_idiomas");
-        while(miResultSet.next()) {
-            System.out.println(miResultSet.getString("id_idioma") + " - " + miResultSet.getString("nombre_idioma"));
+        
+        for (int i= 19; i<23; i++) {
+        	resultSetMensajes.absolute(i);
+        	System.out.println((i-18)+ " - " + resultSetMensajes.getString("mensaje"));
         }
+        
+        /*ResultSet miResultSet = miConexion.createStatement().executeQuery("SELECT * FROM tabla_idiomas");
+        while (miResultSet.next()) {
+            System.out.println(miResultSet.getString("id_idioma") + " - " + miResultSet.getString("nombre_idioma"));
+        }*/
+        
+        elegirIdioma(miConexion);
+    }
+    
+    private static void elegirIdioma(Connection miConexion) throws SQLException {
+    	ResultSet resultSetMensajes = generarMensajes(codigoLenguajeSeleccionado, miConexion);
+    	resultSetMensajes.absolute(24);
+    	System.out.println(resultSetMensajes.getString("mensaje"));
+        int numeroIdioma = lector.nextInt();
+        codigoLenguajeSeleccionado = numeroIdioma;
+        ResultSet resultSetMensajes2 = generarMensajes(codigoLenguajeSeleccionado, miConexion);
+    	resultSetMensajes2.absolute(18);
+    	System.out.println(resultSetMensajes2.getString("mensaje"));
     }
 
-    private static void agregarMensajeNuevo (Connection miConexion) throws SQLException {
+
+    private static void agregarMensajeNuevo(Connection miConexion) throws SQLException {
         System.out.println();
         System.out.print("Ingrese codigo del mensaje: ");
         int codigoDelMensaje = lector.nextInt();
@@ -132,21 +176,27 @@ public class JuegoTateti {
         lector.nextLine();
         String mensaje = lector.nextLine();
 
-        miConexion.createStatement().executeUpdate(String.format("INSERT INTO mensajes values (%d, %d,'%s')", codigoDelMensaje, codigoIdioma, mensaje));
+        miConexion.createStatement().executeUpdate(
+                String.format("INSERT INTO mensajes values (%d, %d,'%s')", codigoDelMensaje, codigoIdioma, mensaje));
 
         System.out.println();
         System.out.print("Mensaje agregado exitosamente!");
     }
 
-    private static void jugar(Connection miConexion) {
-        System.out.println("Ingrese su nombre: ");
-        lector.nextLine(); //Por alguna razon el siguiente nextLine el programa lo ignora y tuve que ponerlo 2 veces
+    private static void jugar(Connection miConexion) throws SQLException {
+    	ResultSet resultSetMensajes = generarMensajes(codigoLenguajeSeleccionado, miConexion);
+    	resultSetMensajes.absolute(2);
+    	System.out.println(resultSetMensajes.getString("mensaje"));
+        lector.nextLine(); // Por alguna razon el siguiente nextLine el programa lo ignora y tuve que
+                           // ponerlo 2 veces
         String nombreJugador = lector.nextLine();
         System.out.println();
 
         TableroTateti tableroTateti = new TableroTateti();
-        System.out.println("Ficha de "+ nombreJugador + ": X");
-        System.out.println("Ficha de la máquina: O");
+        resultSetMensajes.absolute(27);
+        System.out.println(resultSetMensajes.getString("mensaje") + " " + nombreJugador + ": X");
+        resultSetMensajes.absolute(28);
+        System.out.println(resultSetMensajes.getString("mensaje") +": O");
         System.out.println();
 
         Jugador jugadorPersona = new JugadorPersona(FICHA_JUGADOR, nombreJugador);
@@ -154,11 +204,11 @@ public class JuegoTateti {
 
         LocalDateTime fechaYHoraInicio = LocalDateTime.now();
 
-        for(int i = 0; i < 9 && !tableroTateti.hayGanador() ; i++) {
+        for (int i = 0; i < 9 && !tableroTateti.hayGanador(); i++) {
             System.out.println();
             tableroTateti.mostrarTablero();
             System.out.println();
-            if(i%2 == 0) {
+            if (i % 2 == 0) {
                 jugadorPersona.jugar(tableroTateti);
             } else {
                 jugadorComputadora.jugar(tableroTateti);
@@ -167,39 +217,41 @@ public class JuegoTateti {
 
         tableroTateti.mostrarTablero();
 
-        int resultadoPartida = 0; //Se queda en 0 si el jugador pierde
+        int resultadoPartida = 0; // Se queda en 0 si el jugador pierde
 
-        if(tableroTateti.hayGanador()) {
+        if (tableroTateti.hayGanador()) {
             System.out.println("TATETI!!!");
 
-            if(elJugadorEsGanador(tableroTateti)) {
-                resultadoPartida = 1; //Cambia a 1 si se detecta que el jugador gana
-                System.out.println("El ganador es " + nombreJugador);
+            if (elJugadorEsGanador(tableroTateti)) {
+                resultadoPartida = 1; // Cambia a 1 si se detecta que el jugador gana
+                resultSetMensajes.absolute(29);
+                System.out.println(resultSetMensajes.getString("mensaje") + " " + nombreJugador);
             } else {
-                System.out.println("El ganador es la computadora u.u");
+            	resultSetMensajes.absolute(30);
+                System.out.println(resultSetMensajes.getString("mensaje") + "u.u");
             }
         } else {
             System.out.println("Empate");
-            resultadoPartida = 2; //Cambia a 2 si se detecta empate
+            resultadoPartida = 2; // Cambia a 2 si se detecta empate
         }
 
         LocalDateTime fechaYHoraFin = LocalDateTime.now();
 
-        String insertQuery = "INSERT INTO tateti.info_partidas (nombre_jugador, inicio_partida, fin_partida, ganador, idioma_elegido)\n" +
-                "VALUES ('"+ nombreJugador + "', '" + fechaYHoraInicio + "', '" + fechaYHoraFin + "', " + resultadoPartida + ", " + codigoLenguajeSeleccionado + ")";
+        String insertQuery = "INSERT INTO tateti.info_partidas (nombre_jugador, inicio_partida, fin_partida, ganador, idioma_elegido)\n"
+                +
+                "VALUES ('" + nombreJugador + "', '" + fechaYHoraInicio + "', '" + fechaYHoraFin + "', "
+                + resultadoPartida + ", " + codigoLenguajeSeleccionado + ")";
 
         try {
             miConexion.createStatement().executeUpdate(insertQuery);
         } catch (SQLException e) {
-            System.err.println("Hubo un error al guardar los datos de la partida.");
+        	resultSetMensajes.absolute(31);
+            System.out.println(resultSetMensajes.getString("mensaje"));
         }
     }
 
     private static boolean elJugadorEsGanador(TableroTateti tableroTateti) {
         return tableroTateti.getUltimaFichaJugada() == FICHA_JUGADOR;
     }
-
-
-
 
 }
